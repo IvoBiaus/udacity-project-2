990ef3dd01b17a293554399c21d367b2e76b8fe7
Ivo Biaus <ivobiaus@gmail.com>
2021-10-04 20:41:40 -0300

    using helper at middlewares

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,12 +1,9 @@
 import { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
-
-const { JWT_SECRET } = process.env;
+import { getUidFromToken } from '../helpers';
 
 export const verifyAuthToken = (req: Request, res: Response, next: () => void): void => {
   try {
-    const token = req.headers.authorization?.split(' ')[1] || '';
-    jwt.verify(token, `${JWT_SECRET}`);
+    getUidFromToken(req);
     next();
   } catch (error) {
     res.status(401);
@@ -17,9 +14,8 @@ export const verifyIsOwner = (req: Request, res: Response, next: () => void): vo
   const id = parseInt(req.params.id);
 
   try {
-    const token = req.headers.authorization?.split(' ')[1] || '';
-    const decoded = jwt.verify(token, `${JWT_SECRET}`) as { id: number };
-    if (decoded.id !== id) {
+    const tokenId = getUidFromToken(req);
+    if (tokenId !== id) {
       throw new Error('User is not the owner.');
     }
     next();
